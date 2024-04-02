68927fd21211baea4569532b829e425f13ca69d4
Vimal Kurien Sam <vimal.sam@bito.ai>
2024-04-02 21:39:11 +0100

    added api port interface

---
+++ server/auth-service/internals/adaptor/framework/left/routes/auth.routes.go
@@ -0,0 +1,7 @@
+package routes
+
+import "github.com/go-chi/chi/v5"
+
+func (ra *Adaptor) AuthRoutes(r chi.Router) {
+
+}
--- server/auth-service/internals/ports/api.go
+++ server/auth-service/internals/ports/api.go
@@ -1,1 +1,10 @@
 package ports
+
+import "net/http"
+
+type AuthApiPort interface {
+	LoginUserApi(http.ResponseWriter, *http.Request)
+	RegisterUserApi(http.ResponseWriter, *http.Request)
+	UpdateUserApi(http.ResponseWriter, *http.Request)
+	DeleteUserApi(http.ResponseWriter, *http.Request)
+}
