2bf6150ce73a22f5de07b204b6fbb831e198649e
Vimal Kurien Sam <vimal.sam@bito.ai>
2024-04-10 15:26:54 +0100

    connected app to queue and integrated queue docker

--- Makefile
+++ Makefile
@@ -2,4 +2,10 @@ run_agnft_db:
 	@echo "Starting agnft db service..."
 	docker-compose -f ./autogear_nft_deployment/docker-compose.yml stop agnft-auth_pg_db
 	docker-compose -f ./autogear_nft_deployment/docker-compose.yml up --build -d agnft-auth_pg_db
-	@echo "agnft services started..."	
+	@echo "agnft auth db services started..."
+
+run_agnft_queue:
+	@echo "Starting agnft queue service..."
+	docker-compose -f ./autogear_nft_deployment/docker-compose.yml stop agnft-queue
+	docker-compose -f ./autogear_nft_deployment/docker-compose.yml up --build -d agnft-queue
+	@echo "agnft queue services started..."		
--- server/gateway-service/cmd/main.go
+++ server/gateway-service/cmd/main.go
@@ -5,13 +5,21 @@ import (
 	"github.com/vimalkuriensam/auth_gear_nft/gateway-service/internals/adaptors/core/config"
 	"github.com/vimalkuriensam/auth_gear_nft/gateway-service/internals/adaptors/framework/left/routes"
 	"github.com/vimalkuriensam/auth_gear_nft/gateway-service/internals/adaptors/framework/left/server"
+	"github.com/vimalkuriensam/auth_gear_nft/gateway-service/internals/adaptors/framework/right/queue"
 )
 
 func main() {
+	// Initialize config adaptor
 	configPort := config.Initialize()
 	configPort.LoadEnvironment()
+	//initialize, connect and listen to queue messages
+	queuePort := queue.Initialize(configPort)
+	queuePort.Connect()
+	go queuePort.Listen()
+	// Initialize api adaptor
 	apiPort := app.Initialize()
 	routesPort := routes.Initialize(apiPort)
 	serverPort := server.Initialize(configPort, routesPort)
+	// Start the server
 	configPort.GetConfig().Logger.Fatal(serverPort.Serve())
 }
